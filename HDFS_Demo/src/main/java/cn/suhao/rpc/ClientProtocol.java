34a314de401b7e32d18b58ed088bd3c287167da4
suhao <suhao_plus@163.com>
2018-08-09 21:22:35 +0800

    Revert "hdfs_demo"
    hdfs_demo

--- HDFS_Demo/src/main/java/cn/suhao/rpc/ClientProtocol.java
+++
@@ -1,9 +0,0 @@
-package cn.suhao.rpc;
-
-import org.apache.hadoop.ipc.VersionedProtocol;
-
-public interface ClientProtocol extends VersionedProtocol{
-    public static final long versionID = 1L;
-    public String echo(String value) throws Exception;
-    public int add(int v1,int v2) throws Exception;
-}
